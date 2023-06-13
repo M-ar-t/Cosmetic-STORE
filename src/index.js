dca2d246a34c9b8531af9ed2b5018fa9afe26b63
Maria Tkacheva <tkachevamasha35@yandex.ru>
2023-06-13 21:34:07 +0300

    corrections

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import store from './redux/redux-store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <BrowserRouter>
+ <HashRouter>
   <React.StrictMode>
     <Provider store={store}>
         <App />
     </Provider>
   </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
 

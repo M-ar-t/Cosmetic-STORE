f969e5b2c4514d8db95fa27d50c693daf8880876
Maria Tkacheva <tkachevamasha35@yandex.ru>
2023-06-24 23:10:29 +0300

    adapt main about shop

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,7 +5,8 @@ import searchImg from "../../img/Search.png"
 import accountImg from "../../img/account.png"
 import cartImg from "../../img/cart.png"
 import {useDispatch, useSelector} from "react-redux";
-
+import { useState } from 'react';
+import closeBurger from "../../img/closeBtn.png"
 function Nav() {
   
   const dispatch = useDispatch()
@@ -32,66 +33,79 @@ function Nav() {
   const allProducts = () => {
     dispatch({type:"ALL-PRODUCTS"})
   }
+  const[toggle, setToggle] =useState(false)
 
-
+  const burgerMenuShow =() =>{
+    setToggle(!toggle)
+}
   return (
+
     <div className={s.continer}>   
       <ul>
-        <li className={s.container_item}>
+        <li className={s.container_leftList}>
             <NavLink  to="/" ><img src={logoImg} alt='logo'/></NavLink>
         </li>
       </ul>
+      <div className={toggle? s.burgerNone:s.burger}
+      onClick={burgerMenuShow}></div>
 
-      <ul className={s.container_middleList}>
-        <li > 
-              <NavLink  to="/shopall" className={({ isActive }) => isActive ? s.activeLink : s.container_middleItem}>Shop All</NavLink>
-        </li>
-       
-        <li >
-          <NavLink to="/aboutus" className={({ isActive }) => isActive ? s.activeLink : s.container_middleItem}>About us</NavLink>
-        </li>
-        <li >
-          <NavLink to="/blog" className={({ isActive }) => isActive ? s.activeLink : s.container_middleItem}>Blog</NavLink>
-        </li>
-      </ul> 
+     
+      <div className={toggle? [s.continer__inner, s.activeBurgerMenu].join(" "):s.continer__inner}>
+          <div className={toggle? [s.closeBtn, s.activecloseBtn].join(" "):s.closeBtn}
+         onClick={burgerMenuShow}>
+                    <img className={s.closeBtn__pic} 
+                    src={closeBurger} alt=""/>
+          </div>
+          <ul className={s.container_middleList}>
+            <li > 
+                  <NavLink  to="/shopall" className={({ isActive }) => isActive ? s.activeLink : s.container_middleItem}>Shop All</NavLink>
+            </li>
+          
+            <li >
+              <NavLink to="/aboutus" className={({ isActive }) => isActive ? s.activeLink : s.container_middleItem}>About us</NavLink>
+            </li>
+            <li >
+              <NavLink to="/blog" className={({ isActive }) => isActive ? s.activeLink : s.container_middleItem}>Blog</NavLink>
+            </li>
+          </ul> 
 
-      <ul className={s.container_rightList}>
-        <li className={s.container_rightItem}>
-       
-            <button className={s.container_linkBlok} >
-                <img className={s.container_img} src={searchImg} alt='search' onClick={
-                  showInput
-                 
-                }/>
-                {input ? <input className={s.inputSearch} placeholder="Enter the product name"
-                onChange={(e)=>processingEnterSymbol(e)}/> : <p className={s.container_text}>Search</p> } 
-                {!input && allProducts()}
-                
-            </button>
-      
-           
-        </li>
-        <li className={s.container_rightItem}>
-        <button onClick={accountOpen}>
-            <div className={s.container_linkBlok}>
-            <img className={s.container_img} src={accountImg} alt='account'/>
-            <p className={s.container_text}>Account</p> 
-            </div>
-          </button>
+          <ul className={s.container_rightList}>
+            <li className={s.container_rightItem}>
+          
+                <button className={s.container_linkBlok} >
+                    <img className={s.container_img} src={searchImg} alt='search' onClick={
+                      showInput
+                    
+                    }/>
+                    {input ? <input className={s.inputSearch} placeholder="Enter the product name"
+                    onChange={(e)=>processingEnterSymbol(e)}/> : <p className={s.container_text}>Search</p> } 
+                    {!input && allProducts()}
+                    
+                </button>
+          
+              
+            </li>
+            <li className={s.container_rightItem}>
+            <button onClick={accountOpen}>
+                <div className={s.container_linkBlok}>
+                <img className={s.container_img} src={accountImg} alt='account'/>
+                <p className={s.container_text}>Account</p> 
+                </div>
+              </button>
 
-        </li>
-        <li className={s.container_rightItem}>
+            </li>
+            <li className={s.container_rightItem}>
 
-        <button onClick={cartOpen}>
-            <div className={s.container_linkBlok}>
-                <img className={s.container_img} src={cartImg} alt='cart'/>
-                {!cartIsEmpty && <div className={s.fillCart}></div>}
-                <p className={s.container_text}>Cart</p> 
-            </div>
-          </button>
-        </li>
-      </ul> 
-      
+            <button onClick={cartOpen}>
+                <div className={s.container_linkBlok}>
+                    <img className={s.container_img} src={cartImg} alt='cart'/>
+                    {!cartIsEmpty && <div className={s.fillCart}></div>}
+                    <p className={s.container_text}>Cart</p> 
+                </div>
+              </button>
+            </li>
+          </ul> 
+      </div>
       
     </div>
   );
